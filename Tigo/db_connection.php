6bb02a3ce7e48db628e44633ae879c2b5b0e0300
KitsuneMamori <42525541+KitsuneMamori@users.noreply.github.com>
2018-12-13 09:12:25 +0100

    Add files via upload

---
+++ Tigo/db_connection.php
@@ -0,0 +1,36 @@
+<?php
+ 
+function OpenCon()
+ {
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "yZgLvEyzzPd7tZ3D";
+ $db = "Vuurwerkwinkel";
+ 
+ 
+
+	function __construct() {
+		$this->conn = $this->connectDB();
+	}
+	
+	function connectDB() {
+		$conn = mysqli_connect($this->host,$this->user,$this->password,$this->database);
+		return $conn;
+	}
+	
+	function runQuery($query) {
+		$result = mysqli_query($this->conn,$query);
+		while($row=mysqli_fetch_assoc($result)) {
+			$resultset[] = $row;
+		}		
+		if(!empty($resultset))
+			return $resultset;
+	}
+	
+	function numRows($query) {
+		$result  = mysqli_query($this->conn,$query);
+		$rowcount = mysqli_num_rows($result);
+		return $rowcount;	
+	}
+}
+?>   
